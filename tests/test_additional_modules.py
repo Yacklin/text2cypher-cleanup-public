738f51e68889e7d7c4a9e68bd281c620fdd21418
Yacklin Wong <lamkera.hyl@gmail.com>
2025-12-25 14:31:40 -0400

    update tests

--- tests/test_additional_modules.py
+++ tests/test_additional_modules.py
@@ -61,7 +61,7 @@ class TestNeo4jDemoDB(unittest.TestCase):
 class TestConstants(unittest.TestCase):
     def test_constants_exist(self):
         self.assertEqual(constants.ISSUES_COLUMN_NAME, "issues")
-        self.assertIn("CALL apoc.meta.data()", constants.FULL_SCHEMA_CYPHER_QUERY)
+        self.assertIn("CALL db.schema", constants.FULL_SCHEMA_CYPHER_QUERY)
 
 
 class TestLogger(unittest.TestCase):
